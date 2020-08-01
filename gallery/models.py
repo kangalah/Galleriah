68506b48fb0e93f2e2ff609575f793ef8d971220
kangalah <nyongesajoannanjala@gmail.com>
2020-08-01 04:59:18 +0300

    add a gallery-categories table

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class categories(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+
