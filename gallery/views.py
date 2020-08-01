a1c5c80c66c9b8d63b5b4886648deec4c6d33657
kangalah <nyongesajoannanjala@gmail.com>
2020-08-01 13:52:52 +0300

    add the images and search_results view functions

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
+import datetime as dt
+from .models import Gallery
 
 # Create your views here.
+def my_images(request):
+    images = Gallery.objects.all()
+    return render(request, 'index.html', {'images':images})
+
+def single_photo(request, image_id):
+    image = Gallery.objects.get(id=image_id)
+    return render(request, 'pic.html', {'image':image})
+
+def search_results(request):
+    if 'images' in request.GET and request.GET["images"]:
+        search_term = request.GET.get("images")
+        searched_picture = Gallery.search_by_category(search_term)
+        message = f'{search_term}'
+
+        return render(request, 'search.html', {"message":message, "images": searched_picture})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html', {"message":message})
