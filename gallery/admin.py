6fd9871f7adac4128b9aa109ba599372edf6e909
kangalah <nyongesajoannanjala@gmail.com>
2020-08-02 06:00:12 +0300

    create a class articlesadmin for models

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Gallery, categories, location
 
 # Register your models here.
+class ArticlesAdmin(admin.ModelAdmin):
+    filter_horizontal = ('tags',)
+
+    admin.site.register(Gallery)
+    admin.site.register(categories)
+    admin.site.register(location)
