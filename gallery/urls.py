a98cd666148512da677d7433bc9e8759a3108b91
kangalah <nyongesajoannanjala@gmail.com>
2020-08-02 05:33:30 +0300

    add the search views url to the urlpatterns

---
+++ gallery/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url, include
+from . import views
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns=[
+    url(r'^$', views.my_images, name='myimages'),
+    url(r'^images/(\d+)', views.single_photo,name = 'myimage'),
+    url(r'^search/', views.search_results, name = 'search_results'),
+]
