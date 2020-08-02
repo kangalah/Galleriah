fdf7481bd21f46a3a3eb36499d4a04e0d65fbc64
kangalah <nyongesajoannanjala@gmail.com>
2020-08-02 20:57:35 +0300

    add the media url to the url patterns

--- gallery/urls.py
+++ gallery/urls.py
@@ -9,3 +9,5 @@ urlpatterns=[
     url(r'^images/(\d+)', views.single_photo,name = 'myimage'),
     url(r'^search/', views.search_results, name = 'search_results'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
