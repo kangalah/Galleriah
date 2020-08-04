ea4ccc783a95a0169a9c54d9fa586fd828f29e06
kangalah <nyongesajoannanjala@gmail.com>
2020-08-04 04:54:22 +0300

    add save method to the gallerytestcase

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
+from .models import categories,location,Gallery
 
 # Create your tests here.
+class categoriesTestCase(TestCase):
+    def setUp(self):
+        self.travel = categories(name = 'travel')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.travel, categories))
+
+    def test_save_method(self):
+        self.travel.save_category()
+        category = categories.objects.all()
+        self.assertTrue(categories)
+
+# class locationTestCase(TestCase):
+#     def setup(self):
+#         self.Zambia = location(name = 'Zambia')
+
+#     def test_instance(self):
+#         self.assertTrue(isinstance(self.Zambia,location))
+
+#     def test_save_method(self):
+#         self.Zambia.save_location()
+#         locations = location.objects.all()
+#         self.assertTrue(len(locations)>0)
+
+class GalleryTestCase(TestCase):
+    def setUp(self):
+        self.Range = Image(name = 'Range', description = 'car of my dreams')
+        self.Range.save_image()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Range, Image))
+
+    def test_save_method(self):
+        self.Range.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images)>0)
